387dafc368e79583ecf8ca8c216c9c8b5486b034
Karyne Ohara <150279762+KaryneOhara@users.noreply.github.com>
2025-03-11 18:28:55 -0300

    Update notificacao.py

--- models/notificacao.py
+++ models/notificacao.py
@@ -2,21 +2,37 @@ from models import conectar_db
 
 class Notificacao:
         
+ 
+    @classmethod
+    def notificacao_contratante(cls, con_id, arb_id, acao):
+        conn = conectar_db()  
+        cursor = conn.cursor(dictionary=True)
+        cursor.execute("select usu_nome from tb_usuarios join tb_arbitros on arb_usu_id = usu_id where arb_id = %s", (arb_id,))
+        nome = cursor.fetchone()
+        conteudo = f"O árbitro {nome['usu_nome']} {acao} sua solicitação"
+        cursor.execute("select con_usu_id as id from tb_contratantes where con_id = %s", (con_id,))
+        id = cursor.fetchone()
+        cursor.execute("INSERT INTO tb_notificacoes(not_usu_id, not_conteudo) VALUES(%s,%s)", (id, conteudo,))
+        conn.commit()
+        conn.close()
+        cursor.close()
+        return True
+    
+    @classmethod
+    def notificacao_arbitro(cls, con_id, arb_id):
+        conn = conectar_db()  
+        cursor = conn.cursor(dictionary=True)
+        cursor.execute("select usu_nome from tb_usuarios join tb_contratantes on con_usu_id = usu_id where con_id = %s", (con_id,))
+        nome = cursor.fetchone()
+        cursor.execute("select arb_usu_id as id from tb_arbitros where arb_id = %s", (arb_id,))
+        id = cursor.fetchone()
+        conteudo = f"Você tem uma nova solicitação de {nome['usu_nome']}"
+        cursor.execute("INSERT INTO tb_notificacoes(not_usu_id, not_conteudo) VALUES(%s,%s)", (id['id'], conteudo,))
+        conn.commit()
+        conn.close()
+        cursor.close()
+        return True
 
-    # Falta testar, acho que vai dar erro
-    # Por enquanto só presta a de listar    
-    # @classmethod
-    # def nova_notificacao(cls, usu_id, arb_id, acao):
-    #     conn = conectar_db()  
-    #     cursor = conn.cursor(dictionary=True)
-    #     cursor.execute("select usu_nome from tb_usuarios join tb_arbitros on arb_usu_id = usu_id where arb_id = %s", (arb_id,))
-    #     nome_arb = cursor.fetchone()
-    #     conteudo = f"O árbitro {nome_arb} {acao} sua solicitação"
-    #     cursor.execute("INSERT INTO tb_notificacoes(not_usu_id, not_conteudo) VALUES(%s,%s)", (usu_id, conteudo,))
-    #     conn.commit()
-    #     conn.close()
-    #     cursor.close()
-    #     return True
     
     @classmethod
     def listar(cls, usu_id):
