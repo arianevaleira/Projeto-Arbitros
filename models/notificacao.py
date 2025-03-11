eb418f152e29b70335d251c93d47227f5b44cae0
Karyne Ohara <150279762+KaryneOhara@users.noreply.github.com>
2025-03-11 14:54:46 -0300

    Create notificacao.py

---
+++ models/notificacao.py
@@ -0,0 +1,38 @@
+from models import conectar_db
+
+class Notificacao:
+        
+
+    # Falta testar, acho que vai dar erro
+    # Por enquanto só presta a de listar    
+    # @classmethod
+    # def nova_notificacao(cls, usu_id, arb_id, acao):
+    #     conn = conectar_db()  
+    #     cursor = conn.cursor(dictionary=True)
+    #     cursor.execute("select usu_nome from tb_usuarios join tb_arbitros on arb_usu_id = usu_id where arb_id = %s", (arb_id,))
+    #     nome_arb = cursor.fetchone()
+    #     conteudo = f"O árbitro {nome_arb} {acao} sua solicitação"
+    #     cursor.execute("INSERT INTO tb_notificacoes(not_usu_id, not_conteudo) VALUES(%s,%s)", (usu_id, conteudo,))
+    #     conn.commit()
+    #     conn.close()
+    #     cursor.close()
+    #     return True
+    
+    @classmethod
+    def listar(cls, usu_id):
+        conn = conectar_db()
+        cursor = conn.cursor(dictionary=True)
+        cursor.execute("SELECT not_id as id, not_conteudo as conteudo, not_data as data FROM tb_notificacoes join tb_usuarios on usu_id = not_usu_id where usu_id = %s ", (usu_id,))
+        notificacoes = cursor.fetchall()
+        cursor.close()
+        conn.close()
+        return notificacoes
+    
+    @classmethod
+    def delete(cls,id):
+        conn = conectar_db()
+        cursor = conn.cursor()
+        cursor.execute('DELETE FROM tb_notificacoes WHERE not_id = %s', (id,))
+        conn.commit()
+        cursor.close()
+        conn.close()
