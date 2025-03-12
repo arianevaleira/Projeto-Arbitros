67e597c1891a298127276d3d2680a1d1913cf513
Karyne Ohara <150279762+KaryneOhara@users.noreply.github.com>
2025-03-11 22:41:15 -0300

    Update notificacao.py

--- models/notificacao.py
+++ models/notificacao.py
@@ -7,12 +7,12 @@ class Notificacao:
     def notificacao_contratante(cls, con_id, arb_id, acao):
         conn = conectar_db()  
         cursor = conn.cursor(dictionary=True)
-        cursor.execute("select usu_nome from tb_usuarios join tb_arbitros on arb_usu_id = usu_id where arb_id = %s", (arb_id,))
+        cursor.execute("select usu_nome from tb_usuarios join tb_arbitros on arb_usu_id = usu_id where arb_usu_id = %s", (arb_id,))
         nome = cursor.fetchone()
         conteudo = f"O árbitro {nome['usu_nome']} {acao} sua solicitação"
         cursor.execute("select con_usu_id as id from tb_contratantes where con_id = %s", (con_id,))
         id = cursor.fetchone()
-        cursor.execute("INSERT INTO tb_notificacoes(not_usu_id, not_conteudo) VALUES(%s,%s)", (id, conteudo,))
+        cursor.execute("INSERT INTO tb_notificacoes(not_usu_id, not_conteudo) VALUES(%s,%s)", (id['id'], conteudo,))
         conn.commit()
         conn.close()
         cursor.close()
