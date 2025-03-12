bb892589c8fe3d66ccb05e1936506b66e8ea9eee
Karyne Ohara <150279762+KaryneOhara@users.noreply.github.com>
2025-03-11 22:41:55 -0300

    Update solicitacao.py

--- models/solicitacao.py
+++ models/solicitacao.py
@@ -18,8 +18,21 @@ class Solicitacao:
     def listar(cls, id):
         conn = conectar_db()
         cursor = conn.cursor(dictionary=True)
-        cursor.execute("SELECT sol_data as data, sol_inicio as inicio, sol_termino as fim, sol_descricao as descricao, (select usu_nome from tb_usuarios join tb_contratantes on con_usu_id = usu_id where con_id = sol_con_id)  as contratante, sol_con_id as con_id from tb_solicitacoes join tb_arbitros on arb_id = sol_arb_id join tb_usuarios on usu_id = arb_usu_id where usu_id = %s", (id,))
+        cursor.execute('''SELECT sol_id, sol_data as data, sol_inicio as inicio, sol_termino as fim, sol_descricao as descricao, 
+                       (select usu_nome from tb_usuarios join tb_contratantes on con_usu_id = usu_id where con_id = sol_con_id)  as contratante, sol_con_id as con_id, sol_status as status 
+                       from tb_solicitacoes join tb_arbitros on arb_id = sol_arb_id join tb_usuarios on usu_id = arb_usu_id where usu_id = %s 
+                       ORDER BY CASE sol_status WHEN 'Pendente' THEN 1 WHEN 'Aceita' THEN 2 WHEN 'Recusada' THEN 3 END''', (id,))
         solicitacoes = cursor.fetchall()
         cursor.close()
         conn.close()
         return solicitacoes
+    
+    @classmethod
+    def alterar_status(cls,id,status):
+        conn = conectar_db()
+        cursor = conn.cursor(dictionary=True)
+        cursor.execute("UPDATE tb_solicitacoes SET sol_status = %s WHERE sol_id = %s", (status,id,))
+        conn.commit()
+        cursor.close()
+        conn.close()
+        return True
