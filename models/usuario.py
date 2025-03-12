2210c6aca1c50eb86291d586f0b4da4fbff5f29c
Ariane valeria <153088012+arianevaleira@users.noreply.github.com>
2025-03-12 10:19:44 -0300

    Update usuario.py

--- models/usuario.py
+++ models/usuario.py
@@ -3,62 +3,70 @@ from werkzeug.security import generate_password_hash
 from models import conectar_db
 
 class Usuario(UserMixin):
-    _hash : str
+    _hash: str
+
     def __init__(self, **kwargs):
         self._id = None
-        if 'nome' in kwargs.keys():
-            self._nome = kwargs['nome']
-        if 'email' in kwargs.keys():
-            self._email = kwargs['email']
-        if 'cpf' in kwargs.keys():
-            self._cpf = kwargs['cpf']
-        if 'telefone' in kwargs.keys():
-            self._telefone = kwargs['telefone']
-        if 'senha' in kwargs.keys():
-            self._senha = kwargs['senha']
-        if 'hash' in kwargs.keys():
-            self._hash = kwargs['hash']
-        if 'tipo' in kwargs.keys():
-            self._tipo = kwargs['tipo']
-
+        self._cep = None 
+        self._nome = kwargs.get('nome')
+        self._email = kwargs.get('email')
+        self._cpf = kwargs.get('cpf')
+        self._telefone = kwargs.get('telefone')
+        self._tipo = kwargs.get('tipo')
+        self._hash = kwargs.get('hash')
+        if 'senha' in kwargs:
+            self._senha = kwargs['senha']  
+        self._cep = kwargs.get('cep')
+        self._cidade = kwargs.get('cidade')
+        self._estado = kwargs.get('estado')
 
     def get_id(self):
         return str(self._id)
 
-    
     @property
     def _senha(self):
         return self._hash
-    
+
     @_senha.setter
     def _senha(self, senha):
-        self._hash = generate_password_hash(senha)
+        if senha:  
+            self._hash = generate_password_hash(senha)
 
-    def add_usuario(self):        
-        conn = conectar_db()  
-        cursor = conn.cursor(dictionary=True)      
-        cursor.execute("INSERT INTO tb_usuarios(usu_nome, usu_email, usu_cpf, usu_telefone, usu_senha, usu_tipo) VALUES(%s,%s,%s,%s,%s,%s)", (self._nome, self._email, self._cpf, self._telefone, self._hash, self._tipo,))
+    def add_usuario(self):
+        if not self._hash:  # Se a senha não foi definida
+            raise ValueError("Erro: A senha não pode ser nula!")
+
+        conn = conectar_db()
+        cursor = conn.cursor(dictionary=True)
+
+        cursor.execute("""
+            INSERT INTO tb_usuarios (usu_nome, usu_email, usu_cpf, usu_telefone, usu_senha, usu_tipo, usu_cep, usu_cidade, usu_estado) 
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+        """, (self._nome, self._email, self._cpf, self._telefone, self._hash, self._tipo, self._cep, self._cidade, self._estado))
+
         self._id = cursor.lastrowid
         conn.commit()
-        conn.close()
         cursor.close()
+        conn.close()
         return True
-    
+
     @classmethod
-    def get(cls,user_id):
+    def get(cls, user_id):
         conn = conectar_db()
         cursor = conn.cursor(dictionary=True)
         cursor.execute("SELECT * FROM tb_usuarios WHERE usu_id = %s", (user_id,))
         user = cursor.fetchone()
         conn.commit()
         conn.close()
+
         if user:
-            loaduser = Usuario(email=user['usu_email'] , hash=user['usu_senha'])
+            loaduser = Usuario(email=user['usu_email'], hash=user['usu_senha'], nome=user['usu_nome'], 
+                               cpf=user['usu_cpf'], telefone=user['usu_telefone'], tipo=user['usu_tipo'], 
+                               cep=user['usu_cep'], cidade=user['usu_cidade'], estado=user['usu_estado'])
             loaduser._id = user['usu_id']
             return loaduser
-        else:
-            return None
-    
+        return None
+
     @classmethod
     def exists(cls, email):
         conn = conectar_db()
@@ -67,20 +75,14 @@ class Usuario(UserMixin):
         user = cursor.fetchall()
         conn.commit()
         conn.close()
-        if user:
-            return True
-        else:
-            return False
-    
-    
-    
+        return bool(user)
+
     @classmethod
-    def get_by_email(cls,email):
+    def get_by_email(cls, email):
         conn = conectar_db()
         cursor = conn.cursor(dictionary=True)
         cursor.execute("SELECT usu_id, usu_email, usu_senha, usu_tipo FROM tb_usuarios WHERE usu_email = %s", (email,))
-        user = cursor.fetchone() 
+        user = cursor.fetchone()
         conn.commit()
         conn.close()
-    
         return user
